9c66f3b7b0d0124a0030776106b16fadf44245b2
Zed <skhull.coding@proton.me>
2025-03-04 15:26:17 +0530

    chore: duplication in names, no indexing

--- app/models/User.py
+++ app/models/User.py
@@ -1,17 +1,13 @@
-from mongoengine import *
+from mongoengine import Document, StringField
 from env import DatabaseEnv as db
 
 
 class User(Document):
-    name = StringField(required=True, unique=True,
-                       min_length=3, max_length=50)
-    email = StringField(required=True, unique=True, index=True)
+    name = StringField(required=True, min_length=3, max_length=50)
+    email = StringField(required=True, unique=True)
     password = StringField(required=True, min_length=8)
 
     meta = {
         'db_alias': db.CONN,
-        'collection': 'users',
-        'indexes': [
-            'email'
-        ]
+        'collection': 'users'
     }
