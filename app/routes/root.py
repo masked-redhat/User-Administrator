226509f199f24e5b0c398d29a8e32b51edeed048
Zed <skhull.coding@proton.me>
2025-03-04 17:12:20 +0530

    feat: docs endpoint

--- app/routes/root.py
+++ app/routes/root.py
@@ -4,5 +4,10 @@ root_blueprint = Blueprint('root_blueprint', __name__)
 
 
 @root_blueprint.route("/")
-def serve_html():
+def serve_landing():
     return render_template("index.html")
+
+
+@root_blueprint.route("/docs")
+def serve_docs():
+    return render_template("docs.html")
