52fd2778336e82a4b369d5e2f39f9aa40e5d74ba
Zed <skhull.coding@proton.me>
2025-03-04 06:14:39 +0530

    fix: ports are of type integer

--- env/__init__.py
+++ env/__init__.py
@@ -8,13 +8,13 @@ env_vars = load_env(env_file)
 
 
 class ServerEnv:
-    PORT = env_vars.get('PORT', '5000')
+    PORT = int(env_vars.get('PORT', '5000'))
     HOST = env_vars.get('HOST', 'localhost')
 
 
 class DatabaseEnv:
     HOST = env_vars.get('MONGO_HOST', '127.0.0.1')
-    PORT = env_vars.get('MONGO_PORT', '27017')
+    PORT = int(env_vars.get('MONGO_PORT', '27017'))
     DB = env_vars.get('MONGO_DATABASE')
     USER = env_vars.get('MONGO_USER')
     PASSWORD = env_vars.get('MONGO_PASS')
